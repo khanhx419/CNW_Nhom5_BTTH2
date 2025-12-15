4dbfbd6652d139447b86d1ed18eca425950be117
thai1 <ngotuananh2005@gmail.com>
2025-12-15 10:08:14 +0700

    update phần materialcontroller

--- controllers/MaterialController.php
+++ controllers/MaterialController.php
@@ -1,17 +1,32 @@
 <?php
+require_once 'config/Database.php';
+require_once 'controllers/AuthMiddleware.php';
+require_once 'models/Material.php';
+
 class MaterialController {
 
-    public function __construct(){
+    private $materialModel;
+
+    public function __construct() {
         AuthMiddleware::requireInstructor();
-        $this->materialModel = new Material(Database::connect());
+
+        $database = new Database();
+        $db = $database->getConnection();
+
+        $this->materialModel = new Material($db);
     }
 
-    public function upload(){
-        $lesson_id = $_POST['lesson_id'];
-        $file = $_FILES["file"];
+    public function upload() {
 
-        $filename = time() . "_" . $file['name'];
+        $lesson_id = $_POST['lesson_id'];
+        $course_id = $_POST['course_id'];
+        $file = $_FILES['file'];
+
+        // Tạo tên file tránh trùng
+        $filename = time() . "_" . basename($file['name']);
         $path = "assets/uploads/materials/" . $filename;
+
+        // Upload file
         move_uploaded_file($file['tmp_name'], $path);
 
         $data = [
@@ -23,6 +38,8 @@ class MaterialController {
 
         $this->materialModel->upload($data);
 
-        header("Location: index.php?controller=Lesson&action=manage&course_id=".$_POST['course_id']);
+        // Redirect đúng router
+        header("Location: /CNW_Nhom5_BTTH2/lesson/manage?course_id=" . $course_id);
+        exit();
     }
 }
