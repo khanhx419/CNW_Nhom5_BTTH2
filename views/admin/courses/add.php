575eec259723b37a42e69e729c1a5e4c8a702333
thai1 <ngotuananh2005@gmail.com>
2025-12-15 10:19:55 +0700

    update

--- views/admin/courses/add.php
+++ views/admin/courses/add.php
@@ -1,6 +1,22 @@
 <?php
 $custom_css = '<link rel="stylesheet" href="/CNW_Nhom5_BTTH2/assets/css/add_khoahoc.css">';
 include 'views/layouts/header.php';
+
+/*
+|--------------------------------------------------
+| Danh mục mẫu (dùng khi chưa load từ DB)
+|--------------------------------------------------
+*/
+$sampleCategories = [
+    ['id' => 1, 'name' => 'Lập trình Web'],
+    ['id' => 2, 'name' => 'Java'],
+    ['id' => 3, 'name' => 'Ngoại ngữ'],
+    ['id' => 4, 'name' => 'Học máy'],
+    ['id' => 5, 'name' => 'Trí tuệ nhân tạo']
+];
+
+// Nếu controller chưa truyền $categories thì dùng danh mục mẫu
+$categories = $categories ?? $sampleCategories;
 ?>
 
 <div class="container mt-4">
@@ -16,11 +32,13 @@ include 'views/layouts/header.php';
             <label class="form-label">Danh mục</label>
             <select name="category_id" class="form-control" required>
                 <option value="">-- Chọn danh mục --</option>
+
                 <?php foreach ($categories as $cat): ?>
                     <option value="<?= $cat['id'] ?>">
                         <?= htmlspecialchars($cat['name']) ?>
                     </option>
                 <?php endforeach; ?>
+
             </select>
         </div>
 
